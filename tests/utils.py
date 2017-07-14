fa5a93a8d4ceb9c2443de700a6b5be8edc327250
Pepijn Dragt <pepijn.dragt@gmail.com>
2017-07-14 18:00:13 +0200

    remove f strings for python 35

--- tests/utils.py
+++ tests/utils.py
@@ -1,7 +1,8 @@
 import sys
 import os
 
+
 def get_file(filename):
     working_path = sys.modules[__name__].__file__
     working_dir = os.path.dirname(working_path)
-    return f'{working_dir}/test_files/{filename}'
+    return '{}/test_files/{}'.format(working_dir, filename)
